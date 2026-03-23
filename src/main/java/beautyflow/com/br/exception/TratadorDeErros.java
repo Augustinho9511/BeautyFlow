ee428844f2518030e498835949b9d398d4b71d57
Pedro Augusto <pedroaugusto159159@gmail.com>
2026-03-23 21:17:06 +0100

    feat: implementa TratadorDeErros global com RestControllerAdvice

---
+++ src/main/java/beautyflow/com/br/exception/TratadorDeErros.java
@@ -0,0 +1,33 @@
+package beautyflow.com.br.exception;
+
+
+import beautyflow.com.br.model.dto.DadosErroValidacao;
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.List;
+
+@RestControllerAdvice
+public class TratadorDeErros {
+
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity tratarErro404() {
+        return ResponseEntity.notFound().build();
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity tratarErro400(MethodArgumentNotValidException ex) {
+        List<DadosErroValidacao> erros = ex.getFieldErrors().stream()
+                .map(DadosErroValidacao::new)
+                .toList();
+        return ResponseEntity.badRequest().body(erros);
+    }
+
+    @ExceptionHandler(RegraDeNegocioException.class)
+    public ResponseEntity tratarErroRegraDeNegocio(RegraDeNegocioException ex) {
+        return ResponseEntity.badRequest().body(ex.getMessage());
+    }
+}
---
+++ src/main/java/beautyflow/com/br/model/dto/DadosErroValidacao.java
@@ -0,0 +1,10 @@
+package beautyflow.com.br.model.dto;
+
+import org.springframework.validation.FieldError;
+
+public record DadosErroValidacao(String campo, String mensagem) {
+
+    public DadosErroValidacao(FieldError erro){
+        this(erro.getField(), erro.getDefaultMessage());
+    }
+}
