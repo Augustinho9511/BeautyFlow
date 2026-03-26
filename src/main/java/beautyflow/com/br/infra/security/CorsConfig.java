76c3f4a45a24f7583535bd838ed50f2d4253684c
Pedro Augusto <pedroaugusto159159@gmail.com>
2026-03-26 15:22:20 +0100

    libera o CORS para o frontend

---
+++ src/main/java/beautyflow/com/br/infra/security/CorsConfig.java
@@ -0,0 +1,17 @@
+package beautyflow.com.br.infra.security;
+
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+public class CorsConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**") // Libera TODAS as rotas da sua API
+                .allowedOrigins("http://localhost:5173") // O endereço do nosso React!
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
+
+}
